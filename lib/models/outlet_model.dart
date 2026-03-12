a21ba5ea5347e3e26856fe008329635c11ab2a5e
Anooj k <anoojk511@gmail.com>
2026-03-12 19:05:47 +0530

    Update outlet_model.dart

---
+++ lib/models/outlet_model.dart
@@ -0,0 +1,17 @@
+class OutletModel {
+  final String id;
+  final String name;
+  final String rssUrl;
+  final String category;
+  final String language;
+  final String emoji;
+
+  const OutletModel({
+    required this.id,
+    required this.name,
+    required this.rssUrl,
+    required this.category,
+    required this.language,
+    required this.emoji,
+  });
+}
